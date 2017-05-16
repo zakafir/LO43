dc92bb6585950c040a1a484a4b87ed7f412e5474
zakafir <zak.afir@gmail.com>
2017-05-16 07:32:20 +0200

    Adding Gantt chart

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,23 @@
 
 //import services.Core.Worker;
 
+import View.DrawGantt;
+import org.jfree.ui.RefineryUtilities;
+
 import java.sql.SQLException;
 
 /** Assumes UTF-8 encoding. JDK 7+. */
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        //Database.createCoreDatabase();
         Retriever retriever = new Retriever();
         retriever.readSolutionAndInstanceFiles();
-        //retriever.readInstance();
-        //worker
-        /*Worker worker1 = new Worker(4);
-        worker1.getWorkerData();
+        final DrawGantt drawGantt = new DrawGantt("Gantt Chart Demo 1");
+        drawGantt.pack();
+        RefineryUtilities.centerFrameOnScreen(drawGantt);
+        drawGantt.setVisible(true);
 
-        Worker worker2 = new Worker(9);
-        worker2.getWorkerData();
 
-        Worker worker3 = new Worker(5);
-        worker3.getWorkerData();*/
     }
 }
 
